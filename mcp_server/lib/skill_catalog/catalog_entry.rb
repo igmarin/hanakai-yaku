3d2fe9f98dddd1bbd0fcd2f746a9507a327e395d
Ismael G Marin C <ismael.marin@gmail.com>
2026-05-23 01:13:09 -0600

    Clean start

--- mcp_server/lib/skill_catalog/catalog_entry.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Lightweight catalog entry for listing skills without loading full content.
-#
-# @!attribute [r] name [String] The canonical skill name from frontmatter
-# @!attribute [r] description [String] Brief description of the skill
-# @!attribute [r] category [String] Either "skills" or "agents"
-# @!attribute [r] ecosystem_sources [Array<String>] List of external dependencies
-CatalogEntry = Data.define(:name, :description, :category, :ecosystem_sources)
